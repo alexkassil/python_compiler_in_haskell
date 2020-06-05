8875cc168440c642330d369f898c50cff73813eb
Alex Kassil <alexkassil@sbcglobal.net>
2020-06-04 23:50:14 -0700

    Complete JSon renderer

--- haskell/JSON.hs
+++ haskell/JSON.hs
@@ -32,3 +32,40 @@ getArray (JArray a)   = Just a
 getArray _            = Nothing
 
 isNull v              = v == JNull
+
+example = JObject [("baz",
+                    JArray [
+                       JObject [("foo", JNumber 1), ("bar", JBool False)],
+                       JObject [("foo", JNumber 1), ("bar", JBool False)],
+                       JBool True,
+                       JBool False,
+                       JNull,
+                       JString "1",
+                       JNumber 1,
+                       JNumber 2.5
+                       ])]
+
+genRender :: JValue -> String
+genRender (JString s) = show s
+genRender (JNumber d) = show d
+genRender (JBool b) = show b
+genRender JNull     = "null"
+genRender (JArray []) = "[]"
+genRender (JArray (x:xs)) = "[" ++ (genRender x) ++ (genRenderArrayHelper xs) ++ "]"
+genRender (JObject []) = "{}"
+genRender (JObject (x:xs)) = "{" ++ (show (fst x)) ++ ":" ++ (genRender (snd x)) ++ (genRenderObjectHelper xs) ++ "}"
+--render _ = ""
+
+genRenderArrayHelper :: [JValue] -> String
+genRenderArrayHelper [] = ""
+genRenderArrayHelper (x:xs) = "," ++ genRender x ++ genRenderArrayHelper xs
+
+genRenderObjectHelper :: [(String, JValue)] -> String
+genRenderObjectHelper [] = ""
+genRenderObjectHelper (x:xs) = "," ++ (show (fst x)) ++ ":" ++ (genRender (snd x)) ++ (genRenderObjectHelper xs)
+
+
+render :: JValue -> IO()
+render x = putStrLn $ genRender x
+
+
